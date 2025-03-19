7d2e08c1a319e072b8d62fa5170b70d345fa24ab
mingyu <mingyu@mtn.co.kr>
2025-03-19 17:39:00 +0900

    #init, spring boot project install and dependency add google cloud speech

---
+++ src/test/java/com/mingyu/mtn/speechtotext/SpeechToTextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mingyu.mtn.speechtotext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeechToTextApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
